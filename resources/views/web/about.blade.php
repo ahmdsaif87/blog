5ae071146594fe0f0654db2b57aa1974f1ecee6a
ahmadsaif87 <ahmadsaif7456@gmail.com>
2025-03-30 09:49:58 +0700

    gjd

--- resources/views/web/about.blade.php
+++ resources/views/web/about.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
     <div class="flex flex-col items-center text-center mx-auto mb-6">
-        <h1 class="text-2xl font-bold mb-2">Contact</h1>
+        <h1 class="text-2xl font-bold mb-2">About</h1>
     </div>
 </x-layout>
