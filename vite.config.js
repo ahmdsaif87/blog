a1b583e9cceaa91773e5d9093a2f3e74703f1aa6
ahmadsaif87 <ahmadsaif7456@gmail.com>
2025-03-31 17:04:31 +0700

    okok

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,7 @@ export default defineConfig({
         tailwindcss(),
     ],
     build: {
-        // outDir: 'public/build',
-        manifest: true,
-        emptyOutDir: true,
+        outDir: 'dist'
       },
     server: {
         hmr: {
