a71f635c160d9c053d4c3c9917f99fe88d83a38a
ahmadsaif87 <ahmadsaif7456@gmail.com>
2025-03-31 21:36:44 +0700

    okok

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,15 +5,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Saif Dev')</title>
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
+    {{-- @vite('resources/css/app.css')
+    @vite('resources/js/app.js') --}}
+
+    @if (app()->environment('local'))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+        @php
+            $manifest = file_exists(public_path('build/manifest.json'))
+                ? json_decode(file_get_contents(public_path('build/manifest.json')), true)
+                : [];
+        @endphp
+
+        @isset($manifest['resources/css/app.css'], $manifest['resources/js/app.js'])
+            <link rel="stylesheet" href="{{ asset('build/' . $manifest['resources/css/app.css']['file']) }}">
+            <script src="{{ asset('build/' . $manifest['resources/js/app.js']['file']) }}" defer></script>
+        @else
+            <p style="color: red;">Error: manifest.json tidak ditemukan.</p>
+        @endisset
+    @endif
+
+
     <script>
         if (localStorage.getItem('theme') === 'dark') {
-          document.documentElement.classList.add('dark');
-          document.documentElement.style.backgroundColor = '#000000';
+            document.documentElement.classList.add('dark');
+            document.documentElement.style.backgroundColor = '#000000';
         }
-      </script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    </script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body class="min-h-screen flex flex-col">
