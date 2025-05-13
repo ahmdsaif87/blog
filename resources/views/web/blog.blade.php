f99119a5fc566bea52ede3bee5d49d64ded16869
ahmadsaif87 <ahmadsaif7456@gmail.com>
2025-05-13 09:17:04 +0700

    fix the blog

--- resources/views/web/blog.blade.php
+++ resources/views/web/blog.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <div class="space-y-6 px-4 mt-6">
+    <div class="flex flex-col items-center mx-auto mb-6 gap-5 mt-6">
         <h4 class="text-center">Numbers of blogs: {{ count($blogs) }}</h4>
         <div class="space-y-4">
             @foreach ($blogs as $blog)
