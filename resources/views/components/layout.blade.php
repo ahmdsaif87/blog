432c802d55948080fd484cb32db7466c1868b4aa
ahmadsaif87 <ahmadsaif7456@gmail.com>
2025-05-13 09:10:36 +0700

    fix the blog

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Saif Dev')</title>
-    {{-- @vite('resources/css/app.css')
-    @vite('resources/js/app.js') --}}
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 
     @if (app()->environment('local'))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
