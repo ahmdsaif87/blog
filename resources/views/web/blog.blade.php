9097a85771fe059d88d5395149623b6c8e77df94
ahmadsaif87 <ahmadsaif7456@gmail.com>
2025-05-13 10:48:07 +0700

    fix the blog layout

--- resources/views/web/blog.blade.php
+++ resources/views/web/blog.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
-    <div class="flex flex-col items-center mx-auto mb-6 gap-5 mt-6">
-        <h4 class="text-center">Numbers of blogs: {{ count($blogs) }}</h4>
+    <div class="flex flex-col items-center mx-auto mb-6 gap-8">
+        <h4 class="text-center mt-6">Numbers of blogs: {{ count($blogs) }}</h4>
         <div class="space-y-4">
             @foreach ($blogs as $blog)
                 <x-blog-card :blog="$blog" />
