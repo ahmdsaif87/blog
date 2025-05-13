9ed3d4e76278dfaea87e06ce6afbecd35b4a45ac
ahmadsaif87 <ahmadsaif7456@gmail.com>
2025-05-13 14:08:28 +0700

    commit

--- resources/views/web/blog.blade.php
+++ resources/views/web/blog.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-<div class="flex flex-col items-center mx-auto mb-6 mt-6 gap-8 max-w-3xl">
+<div class="flex flex-col items-center mx-auto mb-6 mt-6 gap-8">
         <h4 class="text-center">Numbers of blogs: {{ count($blogs) }}</h4>
         <div class="space-y-4 mt-4">
             @foreach ($blogs as $blog)
--- resources/views/web/blog_detail.blade.php
+++ resources/views/web/blog_detail.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <article class="prose dark:prose-invert text-justify max-w-3xl">
+    <article class="prose dark:prose-invert text-justify">
         <div class="p-4">
             {!! $content !!}
         </div>
