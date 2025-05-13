159046cbfd5e76a7a4e49137df8e4a04bdcbfebb
ahmadsaif87 <ahmadsaif7456@gmail.com>
2025-05-13 09:14:06 +0700

    fix the blog

--- resources/views/web/blog.blade.php
+++ resources/views/web/blog.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <div class="space-y-6 px-4 mt-6">
-        <h5 class="text-center text-lg font-medium">Jumlah blog: {{ count($blogs) }}</h5>
+        <h4 class="text-center">Numbers of blogs: {{ count($blogs) }}</h4>
         <div class="space-y-4">
             @foreach ($blogs as $blog)
                 <x-blog-card :blog="$blog" />
