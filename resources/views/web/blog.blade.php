d225298295de69511510616bd43766cd056d6688
ahmadsaif87 <ahmadsaif7456@gmail.com>
2025-05-13 10:56:22 +0700

    fix the blog

--- resources/views/web/blog.blade.php
+++ resources/views/web/blog.blade.php
@@ -1,6 +1,7 @@
 <x-layout>
-    <div class="flex flex-col items-center mx-auto mt-6 gap-8">
-        <div class="space-y-4 mt-6">
+    <div class="flex flex-col items-center mx-auto mb-6 mt-6 gap-8">
+        <h4 class="text-center">Numbers of blogs: {{ count($blogs) }}</h4>
+        <div class="space-y-4 mt-4">
             @foreach ($blogs as $blog)
                 <x-blog-card :blog="$blog" />
             @endforeach
